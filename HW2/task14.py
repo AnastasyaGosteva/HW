8424e4786346e43a66b527ebb72642546fde1691
Anastasiya Gosteva <gosteva.anastasiya17@yandex.ru>
2023-09-19 02:36:26 +0700

    Добавила решения задач

--- HW2/task14.py
+++ HW2/task14.py
@@ -1,1 +1,8 @@
 # Требуется вывести все целые степени двойки (т.е. числа вида 2k), не превосходящие числа N.
+
+number = int(input("Введите N: "))
+degree = 2
+for i in range(1, number):
+    multi_value = degree ** i
+    if multi_value < number:
+        print (i)
